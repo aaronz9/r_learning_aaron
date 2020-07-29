db466f1f416aff4905842e4a32784efdcb272ff1
Aaron Fernandez <35059964+aaronz9@users.noreply.github.com>
2020-07-29 20:51:00 +1000

    Add files via upload

---
+++ data_preprocessing_template.R
@@ -0,0 +1,68 @@
+
+
+#import the dataset
+
+dataset  = read.csv('Data.csv')
+
+#missing data 
+dataset$Age = ifelse (is.na(dataset$Age),
+                      
+                      ave(dataset$Age, FUN = function(x) mean(x,na.rm = TRUE))
+                      
+                      ,dataset$Age) 
+
+dataset$Salary = ifelse (is.na(dataset$Salary),
+                      
+                      ave(dataset$Salary, FUN = function(x) mean(x,na.rm = TRUE))
+ 
+                           ,dataset$Salary) 
+#Encode the categorical data
+# c is our vector in r 
+
+
+dataset$Country = factor(dataset$Country, levels =  c('France','Spain', 'Germany'), 
+                         
+                         labels = c(1,2,3)
+                         
+                    )
+
+
+dataset$Purchased = factor(dataset$Purchased, levels =  c('Yes','No'), 
+                              labels = c(1,0)
+                         )
+
+#Splitting the data into test set and training set 
+
+# install.packages('caTools') #we commeneted cause we don't want it re-installing
+library(caTools)
+
+set.seed(123)
+
+
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+
+
+training_set = subset(dataset, split == TRUE)
+
+testing_set = subset(dataset, split == FALSE)
+
+
+
+#Feature Scaling
+
+training_set [,2:3] = scale(training_set[,2:3])
+
+testing_set [,2:3]= scale(testing_set[,2:3])
+
+
+
+
+
+
+
+
+
+
+
+
+
